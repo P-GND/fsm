f5af662e07b71db5eee86d1208a26f3b81dcde3f
RobertoFioravanti <133392154+RobertoFioravanti@users.noreply.github.com>
2023-11-29 18:50:36 +0100

    Update test_dummy.c

--- test/test_dummy.c
+++ test/test_dummy.c
@@ -1,7 +1,8 @@
-#include "unity.h"
-#include "def.h"
+#ifdef TEST
+    #include <stdint.h>
+    #include "unity.h"
+    #include "def.h"
 
-//extern void DelayMs(uint8_t ms);
 
 uint8_t tick;
 
@@ -15,9 +16,9 @@ void tearDown(void)
 
 void test_dummy(void)
 {
-    //isElapsed_IgnoreAndReturn(1);
     tick = 0U;
 
-    uint8_t ret = isElapsed(0,1);
+    uint8_t ret = isElapsed(0, 1);
     TEST_ASSERT_EQUAL_UINT8(0, ret);
 }
+#endif
